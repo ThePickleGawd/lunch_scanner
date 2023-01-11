8e085dbab4bcc4532a1e9d14abc5179c77cc0543
Dylan Lu <dylanelu@gmail.com>
2023-01-10 20:03:37 -0800

    int for hashing. works now

--- src/non_bt/lunch_manager.c
+++ src/non_bt/lunch_manager.c
@@ -28,8 +28,8 @@
 
 
 ATM_LOG_LOCAL_SETTING("lunch_manager", D);
-KHASH_SET_INIT_STR(check_in)
-KHASH_MAP_INIT_STR(rssi, rssi_array_t)
+KHASH_SET_INIT_INT(check_in)
+KHASH_MAP_INIT_INT(rssi, rssi_array_t)
 
 #define I2C_CLK_100K 100000
 
@@ -46,10 +46,22 @@ static khash_t(rssi) *rssi_map;
  *******************************************************************************
  */
 
+static int student_id_to_int(nvds_lunch_data_t data)
+{
+    int num = 0;
+    for(int i = STUDENT_ID_ARR_LEN - 1; i >= 0; i--) {
+        num += data.student_id[i] - '0';
+        num *= 10;
+    }
+    
+    return num;
+}
+
 void check_in_student(nvds_lunch_data_t data) {
     // Place in hash map
     int ret;
-    kh_put(check_in, check_in_set, (char *) &data.student_id[0], &ret);
+    kh_put(check_in, check_in_set, student_id_to_int(data), &ret);
+
     if(!ret) ATM_LOG(E, "Error %d - Something with khash", ret);
 
     // Send to school lunch software via I2C to FT260
@@ -65,17 +77,17 @@ void check_in_student(nvds_lunch_data_t data) {
 }
 
 bool student_is_checked_in(nvds_lunch_data_t data) {
-    khiter_t k = kh_get(check_in, check_in_set, (char *) &data.student_id[0]);
+    khiter_t k = kh_get(check_in, check_in_set, student_id_to_int(data));
     return !(k == kh_end(check_in_set));
 }
 
 void receive_rssi(nvds_lunch_data_t data, int rssi)
 {
-    khiter_t k = kh_get(rssi, rssi_map, (char *) &data.student_id[0]);
+    khiter_t k = kh_get(rssi, rssi_map, student_id_to_int(data));
     if (k == kh_end(rssi_map)) {
         // New entry in rssi map
         int ret;
-        khiter_t iter = kh_put(rssi, rssi_map, (char *) &data.student_id[0], &ret);
+        khiter_t iter = kh_put(rssi, rssi_map, student_id_to_int(data), &ret);
 
         // Place new rssi array entry into map
         rssi_array_t rssi_arr;
